ec39aef037900be0cfc371426a6820c3034f6deb
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-25 09:27:29 -0600

    Inicio programa

---
+++ Actividad 8.py
@@ -0,0 +1,7 @@
+productos=[]
+
+while True:
+    print("1.Agregar producto a lista:")
+    print("2.Modificar un producto existente:")
+    print("3.Eliminar un producto")
+    print("4.Ver todos lo productos:")
